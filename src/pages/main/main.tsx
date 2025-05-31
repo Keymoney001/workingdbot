6a196471ce2ffc87a41df264774729952afdffa5
Keymoney001 <kimanimichaek40@gmail.com>
2025-05-31 11:05:46 +0000

    {commit_message}

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -212,9 +212,9 @@ const AppWrapper = observer(() => {
                         <div label={<><AnalysisToolIcon /><Localize i18n_default_text='Analysis Tool' /></>} id='id-analysis-tool'>
                             <iframe src='https://binaryfx.site/api_binaryfx' width='100%' height='500px' frameBorder='0'></iframe>
                         </div>
-                        <div label={<><SignalsIcon /><Localize i18n_default_text='Signals' /></>} id='id-signals'>
-                            <iframe src='signals' width='100%' height='500px' frameBorder='0'></iframe>
-                        </div>
+                      {/* <div label={<><SignalsIcon /><Localize i18n_default_text='Signals' /></>} id='id-signals'>
+                        <iframe src='signals' width='100%' height='500px' frameBorder='0'></iframe>
+                       </div> */}
                         
                         <div label={<><FreeBotsIcon /><Localize i18n_default_text='Free Bots' /></>} id='id-free-bots'>
                             <div className='free-bots'>
